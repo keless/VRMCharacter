53ceafb9b59a7eec9bdfcb34c3313e8799e5b1dd
Paul Zirkle <keless@gmail.com>
2026-07-12 13:49:46 -0700

    Orbit around character origin instead of camera target

--- src/scenes/CharacterScene.tsx
+++ src/scenes/CharacterScene.tsx
@@ -83,7 +83,7 @@ export default function CharacterScene({
 
         {/* Camera Controls */}
         <OrbitControls
-          target={[offset, 1.0, 0]}
+          target={[0, 0, 0]}
           minDistance={1.5}
           maxDistance={6}
           minPolarAngle={Math.PI * 0.1}
